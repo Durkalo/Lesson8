182012eadf63cf797525ca5c3f7595a76a9393ea
KaterynaBagrynyvska <kateryna.durkalo@gmail.com>
2023-04-08 16:29:51 +0300

    add function in branch test

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ def mane_function(dic1,dic2):
 dic1={1:"hello", 2:"world"}
 dic2={3:"Python", 4:"Programming"}
 mane_function(dic1,dic2)
+
+def test_function(a,b):
+    if a > b:
+        print(a, 'is maximum')
+    elif a == b:
+        print(a, 'is equal to', b)
